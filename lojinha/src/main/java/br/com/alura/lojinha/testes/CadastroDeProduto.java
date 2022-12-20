6f37f57ae0b63124c41994d5f3f353daf8b29cca
Emerson009 <henriqueemerson023@gmail.com>
2022-12-20 16:56:05 -0300

    consulta

--- lojinha/src/main/java/br/com/alura/lojinha/testes/CadastroDeProduto.java
+++ lojinha/src/main/java/br/com/alura/lojinha/testes/CadastroDeProduto.java
@@ -1,28 +1,45 @@
 package br.com.alura.lojinha.testes;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
+import br.com.alura.lojinha.dao.CategoriaDAO;
+import br.com.alura.lojinha.dao.ProdutoDAO;
 import br.com.alura.lojinha.model.Categoria;
+import br.com.alura.lojinha.model.Produto;
 import br.com.alura.lojinha.util.JPAUtil;
 
 public class CadastroDeProduto {
 	
 	public static void main(String[] args) {
-		Categoria celulares = new Categoria("CELULARES");
+		cadastrarProduto();
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDAO produtoDAO = new ProdutoDAO(em);
 		
+		Produto produto = produtoDAO.buscarPorId(1l);
+		System.out.println(produto.getPreco());
+		
+		List<Produto> buscarTodos = produtoDAO.buscarTodos();
+		buscarTodos.forEach(p -> System.out.println(p.getNome()));
+	}
+
+	private static void cadastrarProduto() {
+		Categoria celulares = new Categoria("CELULARES");
+		Produto celular = new Produto("Xiaomi Redmi 10", "Muito Legal", new BigDecimal("800"), celulares);
 		
 		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDAO produtoDAO = new ProdutoDAO(em);
+		CategoriaDAO categoriaDAO = new CategoriaDAO(em);
+		
 		em.getTransaction().begin();
 		
-		em.persist(celulares);
-		celulares.setNome("XPTO");
+		categoriaDAO.cadastrar(celulares);
+		produtoDAO.cadastrar(celular);
 		
-		em.flush();
-		em.clear();
-		
-		celulares = em.merge(celulares);
-		celulares.setNome("1234");
-		em.flush();
+		em.getTransaction().commit();
+		em.close();
 	}
 
 }
